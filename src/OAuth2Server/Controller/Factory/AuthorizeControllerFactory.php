b227327c4af64b7338fd0e585db0e36006abbbc7
Jay Klehr <jay@diablomedia.com>
2018-02-13 10:38:56 -0700

    better support for 2.x servicemanager

--- src/OAuth2Server/Controller/Factory/AuthorizeControllerFactory.php
+++ src/OAuth2Server/Controller/Factory/AuthorizeControllerFactory.php
@@ -2,12 +2,18 @@
 
 namespace OAuth2Server\Controller\Factory;
 
-use Zend\ServiceManager\Factory\FactoryInterface;
+use Zend\ServiceManager\FactoryInterface;
 use OAuth2Server\Controller\AuthorizeController;
 use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 class AuthorizeControllerFactory implements FactoryInterface
 {
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        return $this($serviceLocator->getServiceLocator(), AuthorizeController::class);
+    }
+
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null) {
         return new AuthorizeController(
             $container->get('OAuth2Server\Server'),
